5dbf924c51dcd54fecbc5a7d967d079835c37e94
Aneesh Neelam <neelam.aneesh@gmail.com>
2017-07-11 16:51:15 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ tar -xzvf sb-signtool-1.0.tar.gz
 mkdir -p /usr/bin
 mkdir -p /usr/lib/systemd/system
 mkdir -p /usr/share/doc/sb-signtool
-mkdir -p /usr/etc/sb-signtool/keyfiles
+mkdir -p /etc/sb-signtool/keyfiles
 
 install -m 755 sb-signtool-$VERSION/usr/bin/sb-signtool-sign /usr/bin/
 install -m 644 sb-signtool-$VERSION/usr/lib/systemd/system/sb-signtool.service /usr/lib/systemd/system/
 install -m 644 sb-signtool-$VERSION/etc/sb-signtool/modules.conf /etc/sb-signtool/
 install -m 644 sb-signtool-$VERSION/etc/sb-signtool/mok.rc /etc/sb-signtool/
-install -m 644 sb-signtool-$VERSION/usr/share/doc/sb-signtool/example_modules.conf /usr/share/doc/sb-signtool
-install -m 644 sb-signtool-$VERSION/usr/share/doc/sb-signtool/README.md /usr/share/doc/sb-signtool
-install -m 644 sb-signtool-$VERSION/usr/share/doc/sb-signtool/ /usr/share/doc/sb-signtool
+install -m 644 sb-signtool-$VERSION/usr/share/doc/sb-signtool/example_modules.conf /usr/share/doc/sb-signtool/
+install -m 644 sb-signtool-$VERSION/usr/share/doc/sb-signtool/README.md /usr/share/doc/sb-signtool/
+install -m 644 sb-signtool-$VERSION/usr/share/doc/sb-signtool/LICENSE /usr/share/doc/sb-signtool/
 
 rm -r sb-signtool-$VERSION
 rm sb-signtool-1.0.tar.gz
